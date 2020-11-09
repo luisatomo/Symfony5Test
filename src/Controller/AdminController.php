08b44916e4f64a64d43f13d206710bf1862daf13
Luis Mendoza <luis@atomoweb.com>
2020-11-09 06:53:30 -0400

    New Event Form

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,12 @@ class AdminController extends AbstractController
      */
     public function index(): Response
     {
+        $event = new Event();
+
+        $form = $this->createForm(EventType::class, $event);
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,12 @@ class EventController extends AbstractController
      */
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $events = $em->getRepository(Event::class)
+            ->findAll();
         return $this->render('event/index.html.twig', [
             'controller_name' => 'EventController',
+            'events' => $events,
         ]);
     }
 }
