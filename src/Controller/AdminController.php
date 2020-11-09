a6d76fd19963ffc600c5eb3d16fbd325585ce655
Luis Mendoza <luis@atomoweb.com>
2020-11-09 08:26:57 -0400

    Edit Delete actions

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,10 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(Request $request): Response
+    public function index(): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $events = $em->getRepository(Event::class)
+            ->findAll();
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'EventController',
+            'events' => $events,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/add", name="admin_add")
+     */
+    public function add(Request $request): Response
     {
         $event = new Event();
 
@@ -32,8 +47,50 @@ class AdminController extends AbstractController
             return $this->redirectToRoute('admin');
         }
 
-        return $this->render('admin/index.html.twig', [
+        return $this->render('admin/edit.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/admin/edit/{id}", name="admin_edit")
+     */
+    public function edit($id, Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $event = $entityManager->getRepository(Event::class)
+            ->findOneBy(['id' => $id]);
+
+        $form = $this->createForm(EventType::class, $event);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $event = $form->getData();
+            $entityManager->persist($event);
+            $entityManager->flush();
+            $this->addFlash('success', 'Event Updated Correctly');
+
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('admin/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/delete/{id}", name="admin_edit")
+     */
+    public function delete($id, Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $event = $entityManager->getRepository(Event::class)
+            ->findOneBy(['id' => $id]);
+
+        $entityManager->remove($event);
+        $entityManager->flush();
+        $this->addFlash('success', 'Event Removed Correctly');
+
+        return $this->redirectToRoute('admin');
     }
 }
