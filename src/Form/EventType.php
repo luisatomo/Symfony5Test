e1152c68bc3f2f283079b2f4a5eb12b3674db8aa
Luis Mendoza <luis@atomoweb.com>
2020-11-09 07:07:16 -0400

    Image Uploader

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class EventType extends AbstractType
 {
@@ -15,7 +16,16 @@ class EventType extends AbstractType
         $builder
             ->add('title')
             ->add('description')
-            ->add('photo')
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => '...',
+                'download_label' => '...',
+                'download_uri' => true,
+                'image_uri' => true,
+                'imagine_pattern' => '...',
+                'asset_helper' => true,
+            ])
             ->add('video')
             ->add('calendar')
             ->add('save', SubmitType::class);
